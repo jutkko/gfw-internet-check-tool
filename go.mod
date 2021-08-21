0506c0a7bd2ef32172b75adedbb29cf9b37ea899
Juto Yu <jutkko@gmail.com>
2021-08-21 14:36:21 +0800

    Use kkdai's library to downs the videos
    - Fix new youtube API to downlaod
    - No more wget dependency
    - Lots of code removed
    - Lots of golang dependency added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gfw-internet-check-tool
 
 go 1.16
 
-require github.com/tidwall/gjson v1.6.8
+require (
+	github.com/kkdai/youtube/v2 v2.7.4
+	github.com/tidwall/gjson v1.6.8
+)
--- main.go
+++ main.go
@@ -1,156 +1,33 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"os/exec"
-	"strconv"
 	"time"
 
-	"github.com/tidwall/gjson"
+	ytdl "github.com/kkdai/youtube/v2/downloader"
 )
 
-const debug = false
 const tempFile = "video.mp4"
-const testTime = 5
-const videoSize = 0.094
-const youtubeAPI = "http://youtube.com/get_video_info?video_id="
 const videoID = "NQ9RtLrapzc"
-const timeout = 5
+const timeout = 3
 
 func main() {
-	client := &http.Client{
-		Timeout: timeout * time.Second,
+	dl := &ytdl.Downloader{}
+	video, err := dl.GetVideo(videoID)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
 	for {
-		run(client)
-
-		<-time.After(1 * time.Second)
-	}
-}
-
-func run(client *http.Client) {
-	dir, err := ioutil.TempDir("", "gfe-internet-check-tool")
-	if err != nil {
-		log.Fatal(errors.New(fmt.Sprintf("failed to get working directory %s", err)))
-	}
-
-	defer os.RemoveAll(dir)
-
-	info, err := getVideoInfo(client)
-	if err != nil {
-		log.Printf("scientific browsing not good, download failed get video info %s, retrying", err.Error())
-		return
-	}
-
-	url, err := getDownloadURLFromVideoInfo(info)
-	if err != nil {
-		log.Printf("scientific browsing not good, download failed get video url %s, retrying", err.Error())
-	}
-
-	cmd := exec.Command("wget",
-		"--limit-rate",
-		strconv.Itoa(needForSpeed(videoSize, testTime))+"K",
-		"--tries",
-		"0",
-		"--timeout",
-		strconv.Itoa(timeout),
-		"-O",
-		dir+"/"+tempFile,
-		url,
-	)
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(errors.New(fmt.Sprintf("failed to get pipe %s", err)))
-	}
-
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	if stdErr, err := ioutil.ReadAll(stderr); err != nil {
-		log.Fatal(err)
-	} else if len(stdErr) != 0 && debug {
-		log.Printf("%s", stdErr)
-	}
-
-	if err := cmd.Wait(); err != nil {
-		log.Printf("scientific browsing not good, download failed wget %s, retrying", err.Error())
-	} else {
-		log.Printf("scientific browsing still good, true internet")
-	}
-}
-
-func needForSpeed(sizeMB, timeSeconds float64) int {
-	return (int)(sizeMB * 1024 / timeSeconds)
-}
-
-func getVideoInfo(client *http.Client) (string, error) {
-	resp, err := client.Get(youtubeAPI + videoID)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func getDownloadURLFromVideoInfo(videoInfo string) (string, error) {
-	answer, err := url.ParseQuery(videoInfo)
-	if err != nil {
-		return "", err
-	}
-
-	status, ok := answer["status"]
-	if !ok {
-		err = fmt.Errorf("no response status found in the server's answer")
-		return "", err
-	}
-	if status[0] == "fail" {
-		reason, ok := answer["reason"]
-		if ok {
-			err = fmt.Errorf("'fail' response status found in the server's answer, reason: '%s'", reason[0])
+		ctx, _ := context.WithTimeout(context.Background(), timeout*time.Second)
+		err := dl.Download(ctx, video, &video.Formats[0], tempFile)
+		if err == nil {
+			log.Printf("scientific browsing still good, true internet")
 		} else {
-			err = errors.New(fmt.Sprint("'fail' response status found in the server's answer, no reason given"))
+			log.Printf("scientific browsing not good, download failed %s, retrying", err.Error())
 		}
 
-		return "", err
+		<-time.After(timeout * time.Second)
 	}
-	if status[0] != "ok" {
-		err = fmt.Errorf("non-success response status found in the server's answer (status: '%s')", status)
-		return "", err
-	}
-
-	// read the streams map
-	playerResponse, ok := answer["player_response"]
-	if !ok {
-		err = errors.New(fmt.Sprint("no stream map found in the server's answer"))
-		return "", err
-	}
-
-	streamingURL := gjson.Get(playerResponse[0], "streamingData.formats.#(url).url")
-
-	url, err := url.Parse(streamingURL.String())
-	if err != nil {
-		err = fmt.Errorf("no url found in the stream")
-		return "", err
-	}
-
-	return url.String(), nil
 }
