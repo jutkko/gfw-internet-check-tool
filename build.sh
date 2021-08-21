b0827432124a4c1ff9f095d4e8e4755bdfb77d11
Juto Yu <jutkko@gmail.com>
2021-08-21 14:34:03 +0800

    Build the files in bin folder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-GOOS=darwin GOARCH=amd64 go build -gcflags "all=-trimpath=$GOPATH" -ldflags "-X main.gitHash=$(git describe --always --long --dirty)" -o gfw-darwin-"$(git describe --always --long --dirty)" main.go
-GOOS=linux GOARCH=amd64 go build -gcflags "all=-trimpath=$GOPATH" -ldflags "-X main.gitHash=$(git describe --always --long --dirty)" -o gfw-linux-"$(git describe --always --long --dirty)" main.go
+GOOS=darwin GOARCH=amd64 go build -gcflags "all=-trimpath=$GOPATH" -ldflags "-X main.gitHash=$(git describe --always --long --dirty)" -o bin/gfw-darwin-"$(git describe --always --long --dirty)" main.go
+GOOS=linux GOARCH=amd64 go build -gcflags "all=-trimpath=$GOPATH" -ldflags "-X main.gitHash=$(git describe --always --long --dirty)" -o bin/gfw-linux-"$(git describe --always --long --dirty)" main.go
 
